19867c0863b08a27306890b1033f5db6967f8b27
manuel <manuel.vincent-sully@treezor.com>
2018-05-04 17:25:17 +0200

    structure ddd

--- src/AppBundle/Ui/Controller/ExperienceController.php
+++ src/AppBundle/Ui/Controller/ExperienceController.php
@@ -11,15 +11,45 @@ namespace AppBundle\Ui\Controller;
 
 use AppBundle\Ui\Controller\AbstractController;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 
 class ExperienceController extends AbstractController
 {
 
     /**
-     * @Route("/admin/experiences", name="admin_experiences")
+     * @Route("/admin/experiences", name="admin_experiences_xp_list")
      */
-    public function indexAction()
+    public function getXpAction()
+    {
+
+        // replace this example code with whatever you need
+        return $this->render('Experience/index.html.twig');
+    }
+
+
+    /**
+     * @Route("/admin/experiences/new-xp", name="admin_experiences_post_xp")
+     */
+    public function postXpAction(Request $request)
+    {
+
+        if(!empty($request->request)){
+            $name   = $request->request->get('name');
+            $techno = $request->request->get('techno');
+
+            $experience = new CreateXp($name, $techno);
+        }
+        dump($request);
+
+        // replace this example code with whatever you need
+        return $this->render('Experience/post_xp.html.twig');
+    }
+
+    /**
+     * @Route("/admin/experiences/new-img", name="admin_experiences_post_img")
+     */
+    public function postImageAction()
     {
         // replace this example code with whatever you need
         return $this->render('Experience/index.html.twig');
